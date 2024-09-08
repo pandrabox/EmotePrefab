aa1703e3675ed596dcecd7b82bf7378f3e15f1bd
pandravrc <162582491+pandravrc@users.noreply.github.com>
2024-09-08 17:29:18 +0900

    名前空間設定し忘れの修正

--- Editor/PanActionLayer.cs
+++ Editor/PanActionLayer.cs
@@ -13,98 +13,101 @@ using static com.github.pandrabox.emoteprefab.runtime.Generic;
 using com.github.pandrabox.emoteprefab.editor;
 
 
-public class PanActionLayer
+namespace com.github.pandrabox.emoteprefab.editor
 {
-    VRCAvatarDescriptor AvatarDescriptor;
-    AnimatorController RootController;
-    AnimatorStateMachine EmoteStateMachine;
-    AnimatorState CurrentState;
-    EmotePrefab CurrentEmotePrefab;
-    int CurrentID;
-    public PanActionLayer(VRCAvatarDescriptor AvatarDescriptor)
+    public class PanActionLayer
     {
-        this.AvatarDescriptor = AvatarDescriptor;
-        RootController = (AnimatorController)(AvatarDescriptor.baseAnimationLayers[3].animatorController);
-        var TopLevelStateMachines = RootController.layers[0].stateMachine.stateMachines;
-        EmoteStateMachine = TopLevelStateMachines.FirstOrDefault(sm => sm.stateMachine.name == "Emote").stateMachine;
-    }
-    public void AddEmote(int EmoteID, EmotePrefab EP)
-    {
-        this.CurrentID=EmoteID;
-        this.CurrentEmotePrefab = EP;
-        if (EP.IsOneShot)
+        VRCAvatarDescriptor AvatarDescriptor;
+        AnimatorController RootController;
+        AnimatorStateMachine EmoteStateMachine;
+        AnimatorState CurrentState;
+        EmotePrefab CurrentEmotePrefab;
+        int CurrentID;
+        public PanActionLayer(VRCAvatarDescriptor AvatarDescriptor)
         {
-            AddOneShotEmote();
+            this.AvatarDescriptor = AvatarDescriptor;
+            RootController = (AnimatorController)(AvatarDescriptor.baseAnimationLayers[3].animatorController);
+            var TopLevelStateMachines = RootController.layers[0].stateMachine.stateMachines;
+            EmoteStateMachine = TopLevelStateMachines.FirstOrDefault(sm => sm.stateMachine.name == "Emote").stateMachine;
         }
-        else
+        public void AddEmote(int EmoteID, EmotePrefab EP)
         {
-            AddLoopEmote();
+            this.CurrentID = EmoteID;
+            this.CurrentEmotePrefab = EP;
+            if (EP.IsOneShot)
+            {
+                AddOneShotEmote();
+            }
+            else
+            {
+                AddLoopEmote();
+            }
         }
-    }
 
-    public AnimatorState GetEmoteState(string name)
-    {
-        return EmoteStateMachine.states.FirstOrDefault(s => s.state.name == name).state;
-    }
-    public void AddLoopEmote()
-    {
-        CurrentState = EmoteStateMachine.AddState($@"E{CurrentID:D3}");
-        CurrentState.motion = CurrentEmotePrefab.Motion;
-        CurrentState.writeDefaultValues = false;
-        TranditionFromPrepare();
-        TranditionToRecovery_LoopHold();
-        TranditionToForceExit();
-    }
-    public void AddOneShotEmote()
-    {
-        CurrentState = EmoteStateMachine.AddState($@"E{CurrentID:D3}");
-        CurrentState.motion = CurrentEmotePrefab.Motion;
-        CurrentState.writeDefaultValues = false;
-        TranditionFromPrepare();
-        TranditionToRecovery_OneShot();
-        TranditionToForceExit();
-    }
-    private void TranditionFromPrepare()
-    {
-        AnimatorState FromState = GetEmoteState("Prepare standing");
-        AnimatorStateTransition T = FromState.AddTransition(CurrentState);
-        T.hasExitTime = false;
-        T.exitTime = 0.75f;
-        T.hasFixedDuration = true;
-        T.duration = 0.25f;
-        T.offset = 0;
-        T.AddCondition(AnimatorConditionMode.Equals, CurrentID,"VRCEmote");
-    }
-    private void TranditionToRecovery_OneShot()
-    {
-        AnimatorState ToState = GetEmoteState("Recovery standing");
-        AnimatorStateTransition T = CurrentState.AddTransition(ToState);
-        T.hasExitTime = true;
-        T.exitTime = 0.75f;
-        T.hasFixedDuration = true;
-        T.duration = 0.25f;
-        T.offset = 0;
-    }
-    private void TranditionToRecovery_LoopHold()
-    {
-        AnimatorState ToState = GetEmoteState("Recovery standing");
-        AnimatorStateTransition T = CurrentState.AddTransition(ToState);
-        T.hasExitTime = false;
-        T.exitTime = 0.75f;
-        T.hasFixedDuration = true;
-        T.duration = 0.25f;
-        T.offset = 0;
-        T.AddCondition(AnimatorConditionMode.NotEqual, CurrentID, "VRCEmote");
-    }
-    private void TranditionToForceExit()
-    {
-        AnimatorState ToState = GetEmoteState("Force Exit");
-        AnimatorStateTransition T = CurrentState.AddTransition(ToState);
-        T.hasExitTime = false;
-        T.exitTime = 0.75f;
-        T.hasFixedDuration = true;
-        T.duration = 0;
-        T.offset = 0;
-        T.AddCondition(AnimatorConditionMode.If, 0, "Seated");
+        public AnimatorState GetEmoteState(string name)
+        {
+            return EmoteStateMachine.states.FirstOrDefault(s => s.state.name == name).state;
+        }
+        public void AddLoopEmote()
+        {
+            CurrentState = EmoteStateMachine.AddState($@"E{CurrentID:D3}");
+            CurrentState.motion = CurrentEmotePrefab.Motion;
+            CurrentState.writeDefaultValues = false;
+            TranditionFromPrepare();
+            TranditionToRecovery_LoopHold();
+            TranditionToForceExit();
+        }
+        public void AddOneShotEmote()
+        {
+            CurrentState = EmoteStateMachine.AddState($@"E{CurrentID:D3}");
+            CurrentState.motion = CurrentEmotePrefab.Motion;
+            CurrentState.writeDefaultValues = false;
+            TranditionFromPrepare();
+            TranditionToRecovery_OneShot();
+            TranditionToForceExit();
+        }
+        private void TranditionFromPrepare()
+        {
+            AnimatorState FromState = GetEmoteState("Prepare standing");
+            AnimatorStateTransition T = FromState.AddTransition(CurrentState);
+            T.hasExitTime = false;
+            T.exitTime = 0.75f;
+            T.hasFixedDuration = true;
+            T.duration = 0.25f;
+            T.offset = 0;
+            T.AddCondition(AnimatorConditionMode.Equals, CurrentID, "VRCEmote");
+        }
+        private void TranditionToRecovery_OneShot()
+        {
+            AnimatorState ToState = GetEmoteState("Recovery standing");
+            AnimatorStateTransition T = CurrentState.AddTransition(ToState);
+            T.hasExitTime = true;
+            T.exitTime = 0.75f;
+            T.hasFixedDuration = true;
+            T.duration = 0.25f;
+            T.offset = 0;
+        }
+        private void TranditionToRecovery_LoopHold()
+        {
+            AnimatorState ToState = GetEmoteState("Recovery standing");
+            AnimatorStateTransition T = CurrentState.AddTransition(ToState);
+            T.hasExitTime = false;
+            T.exitTime = 0.75f;
+            T.hasFixedDuration = true;
+            T.duration = 0.25f;
+            T.offset = 0;
+            T.AddCondition(AnimatorConditionMode.NotEqual, CurrentID, "VRCEmote");
+        }
+        private void TranditionToForceExit()
+        {
+            AnimatorState ToState = GetEmoteState("Force Exit");
+            AnimatorStateTransition T = CurrentState.AddTransition(ToState);
+            T.hasExitTime = false;
+            T.exitTime = 0.75f;
+            T.hasFixedDuration = true;
+            T.duration = 0;
+            T.offset = 0;
+            T.AddCondition(AnimatorConditionMode.If, 0, "Seated");
+        }
     }
 }
