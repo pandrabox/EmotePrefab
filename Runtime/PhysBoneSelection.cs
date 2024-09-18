bc7547b6371ca53fbb28c5d920de5d80a8d87a80
pandravrc <162582491+pandravrc@users.noreply.github.com>
2024-09-19 06:05:22 +0900

    CustomPropertyDrawer Sample

---
+++ Runtime/PhysBoneSelection.cs
@@ -0,0 +1,29 @@
+﻿#if UNITY_EDITOR
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Xml.Linq;
+using com.github.pandrabox.emoteprefab.runtime;
+using nadena.dev.ndmf;
+using UnityEditor;
+using UnityEditor.Animations;
+using UnityEngine;
+using VRC.SDK3.Avatars.Components;
+using VRC.SDK3.Dynamics.PhysBone.Components;
+using static com.github.pandrabox.emoteprefab.runtime.Generic;
+
+namespace com.github.pandrabox.emoteprefab.runtime
+{
+    [Serializable]
+    public class PhysBoneSelection 
+    {
+        [SerializeField]
+        bool All;
+        [SerializeField]
+        List<VRCPhysBone> PhysBones=new List<VRCPhysBone>();
+        [SerializeField]
+        string label;
+    }
+}
+#endif
---
+++ Runtime/TestComponent.cs
@@ -0,0 +1,15 @@
+﻿
+#if UNITY_EDITOR
+using UnityEngine;
+
+namespace com.github.pandrabox.emoteprefab.runtime
+{
+    public class TestComponent : MonoBehaviour
+    {
+        [SerializeField]
+        public PhysBoneSelection ShrinkPhysBone;
+        public PhysBoneSelection Test2;
+        public PhysBoneSelection Test3;
+    }
+}
+#endif
